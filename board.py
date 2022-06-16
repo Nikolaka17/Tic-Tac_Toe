e46613128020a9d982cc12a63473d4861bb953f3
Nikolaka17 <nikolakakealoha@gmail.com>
2022-06-15 19:24:33 -0500

    Add minimax algorithm

--- board.py
+++ board.py
@@ -1,8 +1,33 @@
+from math import inf
+
 class Board:
     def __init__(self):
         self.board = [0, 0, 0, 0, 0, 0, 0, 0, 0] #0 = empty, 1 = X, 2 = O
         self.turn = 1 #Tells which player is going, 1 for x, 2 for o
         self.winner = 0
+    
+    def check_state(self, state): #finds a winner in a format for the mimimax algorithm
+        #check rows
+        for i in range(3):
+            if state[i*3] == state[i*3 + 1] == state[i*3 + 2] != 0:
+                return True, 1 if state[i*3] == 2 else -1
+        
+        #check columns
+        for i in range(3):
+            if state[i] == state[i + 3] == state[i + 6] != 0:
+                return True, 1 if state[i] == 2 else -1
+        
+        #check diagonals
+        if state[0] == state[4] == state[8] != 0:
+            return True, 1 if state[4] == 2 else -1
+        if state[2] == state[4] == state[6] != 0:
+            return True, 1 if state[4] == 2 else -1
+        
+        #check tie
+        if 0 not in state:
+            return True, 0
+        
+        return False, None
     
     def check_winner(self):
         #Check rows
@@ -27,12 +52,44 @@ class Board:
 
         return False, (-1, -1, -1), "N"
     
-    def find_empty(self):
+    def find_empty(self, state):
         for i in range(9):
-            if self.board[i] == 0:
+            if state[i] == 0:
                 yield i
     
     def place(self, pos):
         if self.board[pos] == 0:
             self.board[pos] = self.turn
             self.turn = 3 - self.turn
+    
+    def minimax(self, state, depth, is_max): #computer will always be O (2)
+        done, score = self.check_state(state)
+        if done:
+            return None, score
+        if depth == 0:
+            return None, score
+
+        moves = list(self.find_empty(state))
+        best_move = moves[0]
+
+        if is_max:
+            max_score = -inf
+            for move in moves:
+                new_state = state.copy()
+                new_state[move] = 2
+                new_score = self.minimax(new_state, depth-1, False)[1]
+                if new_score > max_score:
+                    max_score = new_score
+                    best_move = move
+            return best_move, max_score
+        
+        else:
+            min_score = inf
+            for move in moves:
+                new_state = state.copy()
+                new_state[move] = 1
+                new_score = self.minimax(new_state, depth-1, True)[1]
+                if new_score < min_score:
+                    min_score = new_score
+                    best_move = move
+            return best_move, max_score
